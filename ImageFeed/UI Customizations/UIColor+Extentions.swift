331392cfcceb12160f0950a2127648a7e8ba6723
Aleksei Iakushev <av.yakush@gmail.com>
2023-05-06 21:17:56 +0300

    Внесены правки по замечаниям ревьюера к спринту 8

--- ImageFeed/UI Customizations/UIColor+Extentions.swift
+++ ImageFeed/UI Customizations/UIColor+Extentions.swift
@@ -18,23 +18,4 @@ extension UIColor {
     static var ypBlue = { UIColor(named: "YPBlue") ?? UIColor.blue }
 }
 
-final class GradientImageView: UIImageView {
-    
-    let gradientLayer = CAGradientLayer()
-    
-    override func awakeFromNib() {
 
-        gradientLayer.colors = [UIColor(named: "YPBackgroundAlpha20")?.cgColor ?? UIColor.black.cgColor, UIColor.clear.cgColor]
-        gradientLayer.locations = [0.0, 1.0]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
-
-        layer.addSublayer(gradientLayer)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        gradientLayer.frame = CGRect(x: 0, y: bounds.height - 30, width: bounds.width, height: 30)
-    }
-}
