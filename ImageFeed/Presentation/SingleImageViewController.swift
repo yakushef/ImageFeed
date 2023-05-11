d607ec3b5285ce44eb2edba06a3e5cd35c03e86c
Aleksei Iakushev <av.yakush@gmail.com>
2023-05-11 21:36:25 +0300

    Добавлены tab bar, navigation controller и экран для крупного отображения выбранной картинки

---
+++ ImageFeed/Presentation/SingleImageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Aleksey Yakushev on 11.05.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var fullScreenImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+    // MARK: - Navigation
+
+
+}
